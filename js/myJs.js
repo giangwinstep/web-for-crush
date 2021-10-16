ec0b3f7576c3077e71476d0c92b0e3b95f659794
gianglht <gianglht@outlook.com.vn>
2021-10-16 19:49:09 +0700

    Update source

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "Bé ơi!",
+  text2: "Anh có điều này muốn nói bé nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Bé có yêu anh không nào? ^^",
+  text4: "Nếu em không trả lời mà thoát ra tức là muốn làm vợ anh rùi đó nha :v",
+  text5: "Anh mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "lí do bé yêu anh đi :vvvv",
+  text8: "Gửi cho anh <3",
+  text9: "Vì anh làm em nhớ anh cả ngày",
+  text10: "Anh biết mà ^^ Yêu em 300.000 lunn",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Chờ anh nha, dịch ổn anh sẽ xuất hiện bất ngờ để em bớt nhớ anh <3",
 };
 
 $(document).ready(function () {
